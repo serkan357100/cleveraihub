93c0b6a8f2b1808ad80d6e931c02f5a782e0c288
serkan357100 <smartspaws@gmail.com>
2026-01-06 20:00:42 +0400

    Update api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -5,4 +5,16 @@ const api = axios.create({
 });
 
 export const packagesApi = {
-  list: () => api.get('/package
+  list: () => api.get('/packages'),
+  getById: (id: string) => api.get(`/packages/${id}`),
+};
+
+export const dashboardApi = {
+  get: () => api.get('/dashboard'),
+};
+
+export const paymentsApi = {
+  createCheckout: (data: any) => api.post('/payments/checkout', data),
+};
+
+export default api;
