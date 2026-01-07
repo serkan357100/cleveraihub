473ca1e86c0009b1d7e90d0cb4575383279fec7f
serkan357100 <smartspaws@gmail.com>
2026-01-07 21:44:11 +0400

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,13 +2,13 @@ import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
 import ProfessionAnalyzer from "../components/ai/ProfessionAnalyzer";
 import FeaturedPackages from "../components/landing/FeaturedPackages";
+import IndustryCarousel from "../components/landing/IndustryCarousel";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#070B14] text-white">
       <Navbar />
 
-      {/* Background glow */}
       <div className="pointer-events-none absolute inset-0 overflow-hidden">
         <div className="absolute -top-24 left-1/2 h-[420px] w-[420px] -translate-x-1/2 rounded-full bg-cyan-500/15 blur-[90px]" />
         <div className="absolute top-48 right-0 h-[360px] w-[360px] rounded-full bg-blue-500/10 blur-[110px]" />
@@ -17,6 +17,7 @@ export default function Home() {
       <main className="relative">
         <ProfessionAnalyzer />
         <FeaturedPackages />
+        <IndustryCarousel />
       </main>
 
       <Footer />
